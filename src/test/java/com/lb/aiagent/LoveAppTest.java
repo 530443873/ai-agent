e23b66b6e1a2ee4e565eded2eb837be122ad93d2
luobin <lb@itcast.cn>
2025-09-19 11:01:37 +0800

    支持结构化输出

--- src/test/java/com/lb/aiagent/LoveAppTest.java
+++ src/test/java/com/lb/aiagent/LoveAppTest.java
@@ -30,4 +30,12 @@ public class LoveAppTest {
         answer = loveApp.doChat(message, chatId);
         Assertions.assertNotNull(answer);
     }
+
+    @Test
+    void testChatWithReport() {
+        String chatId = UUID.randomUUID().toString();
+        String message = "你好，我是张三，我想让另一半（李四）更爱我，但我不知道该怎么做";
+        LoveApp.LoveReport loveReport = loveApp.doChatWithReport(message, chatId);
+        Assertions.assertNotNull(loveReport);
+    }
 }
